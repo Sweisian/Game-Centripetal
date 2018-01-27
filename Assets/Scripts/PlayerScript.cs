e4165e511b447de43f992d545bbd18676751ff7d
alexst0ne <alexanderstone2019@u.northwestern.edu>
2018-01-27 16:37:08 -0600

    Setup
    -Organized assets into folders
    -Created GameController and PlayerScript
    -Added reset feature

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	[SerializeField] private float speed;
+	private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
