4e4c1ed3bb215257bbe590029610b6cdd38e613d
Sweisian <rswei888@gmail.com>
2018-01-30 20:21:52 -0600

    added simple player to cursor detection

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 	[SerializeField] private float speed;
 	private Vector2 direction;
+    //private Camera playerCamera = Camera.main;
 
 	// Use this for initialization
 	void Start () {
@@ -13,5 +14,16 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	}
+        DrawShotLine();
+        //Debug.Log("Nigeria");
+    }
+
+    void DrawShotLine()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+
+        // Draw a debug line (transform.position = character position)
+        Debug.DrawRay(transform.position, mousePosition - transform.position, Color.red, 0f);
+    }
 }
