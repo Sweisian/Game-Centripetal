e1eda5db1538e3b8e933874aaa9bfc3912482883
Sweisian <rswei888@gmail.com>
2018-02-12 17:46:23 -0600

    some changes to the dust storm, added a trail, some lasso tuning

--- Assets/Scripts/DustStormScript.cs
+++ Assets/Scripts/DustStormScript.cs
@@ -8,18 +8,30 @@ public class DustStormScript : MonoBehaviour
     [SerializeField] private float moveRate;
     [SerializeField] private GameObject thePlayer;
     [SerializeField] private GameController myGameController;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private float startDistFromPlayer;
     //private float thePlayerX;
+
+    void Start()
+    {
+        transform.position = new Vector3(thePlayer.transform.position.x, thePlayer.transform.position.y - startDistFromPlayer, transform.position.z);
+    }
 
     // Update is called once per frame
     void Update ()
 	{
-	    transform.position = new Vector3(thePlayer.transform.position.x, transform.position.y, transform.position.z);
 
-        transform.Translate(moveRate * Vector3.up * Time.deltaTime, Space.World);
+	    transform.position = new Vector3(thePlayer.transform.position.x, transform.position.y, transform.position.z);
+	    transform.Translate(moveRate * Vector3.up * Time.deltaTime, Space.World);
+
+        if (thePlayer.transform.position.y - transform.position.y > maxDistance)
+            transform.position = new Vector3(thePlayer.transform.position.x, thePlayer.transform.position.y - maxDistance, transform.position.z);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        Destroy(other.gameObject);
+
         if (other.gameObject.tag == "Player")
         {
             myGameController.restartGame();
