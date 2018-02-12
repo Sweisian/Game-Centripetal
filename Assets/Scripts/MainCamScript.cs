cbb1ca9586d3b53c0e11b205b4d3697b979a7640
Emily Blackman <emilyblackman2019@u.northwestern.edu>
2018-02-11 22:31:54 -0600

    created prefabs kinda and started setting up procedural gen

--- Assets/Scripts/MainCamScript.cs
+++ Assets/Scripts/MainCamScript.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class MainCamScript : MonoBehaviour
 {
     [SerializeField] Transform target;
+    
 
     void Update()
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        
     }
 }
---
+++ Assets/Scripts/ProceduralGenScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGenScript : MonoBehaviour
+{
+
+    public Vector2 areaOfExistence;
+    private float fieldOfView;
+    private Camera cam;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    
+	    fieldOfView = cam.fieldOfView;
+	    Debug.Log(fieldOfView);
+    }
+}
