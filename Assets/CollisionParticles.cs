f57915244416bced95b6d017c1e015b0e4530638
Sweisian <rswei888@gmail.com>
2018-03-10 15:54:34 -0600

    added particle effect on player collision with objects

---
+++ Assets/CollisionParticles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionParticles : MonoBehaviour {
+
+    public GameObject collisionPrefab;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+
+        if (collision.collider.tag == "Player")
+        {
+            Debug.Log("Trying to create a collsion prefab");
+
+            ContactPoint2D contact = collision.contacts[0];
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+            Vector3 pos = contact.point;
+            Instantiate(collisionPrefab, pos, rot);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Disappear.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour {
+	public float timeUntilDestroy = 2.0f;
+	float m_timePassed = 0.0f;
+
+	void Update () {
+		m_timePassed += Time.deltaTime;
+		if (m_timePassed > timeUntilDestroy) {
+			Destroy (gameObject);
+		}
+	}
+}
